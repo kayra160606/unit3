40767ab17c2b5035f32d90248ae50c4114bb549c
Mohithacker1 <125952416+Mohithacker1@users.noreply.github.com>
2024-03-21 19:15:09 +0530

    Add files via upload

---
+++ unit3_pract2.java
@@ -0,0 +1,14 @@
+package access;
+import p1.*;
+import p2.*;
+import p3.*;
+
+public class unit3_pract2
+{
+    c1 obj1 = new c1();
+    c2 obj2 = new c2();
+    c3 obj3 = new c3();
+    obj1.printc1();
+    obj2.printc2();
+    obj3.printc3();
+}
