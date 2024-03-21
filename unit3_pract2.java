dbc531c6aada7b1fc5d93e3ba3ddb2040d87bd31
Mohithacker1 <125952416+Mohithacker1@users.noreply.github.com>
2024-03-21 19:21:06 +0530

    Update unit3_pract2.java

--- unit3_pract2.java
+++ unit3_pract2.java
@@ -4,11 +4,15 @@ import p2.*;
 import p3.*;
 
 public class unit3_pract2
-{
+{   
+    public static void main(string args[])
+    {
     c1 obj1 = new c1();
     c2 obj2 = new c2();
     c3 obj3 = new c3();
     obj1.printc1();
     obj2.printc2();
     obj3.printc3();
+    }
 }
+    
