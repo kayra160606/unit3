a8d38c9bc3b5414421b3542f7b4c1e35db313f98
Mohithacker1 <125952416+Mohithacker1@users.noreply.github.com>
2024-03-21 19:35:21 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,9 @@
+package pack1;
+public class A
+{
+    public int a=10;
+    public void printA()
+    {
+        System.out.println("package pack1,classA and method printA");
+    }
+}
---
+++ unit3_pract3.java
@@ -0,0 +1,13 @@
+package pack2;
+import pack1.*;
+
+public class unit3_pract3
+{
+    public static void main(String []args)
+    {
+        A a1 = new A();
+        System.out.println("\n Instance variable "+a1.a);
+        System.out.println("\n Instance method calling");
+        a1.printA();
+    }
+}
